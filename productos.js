c5835922d5bfdeaf41852412d15f943e515427c6
ArnauMedinaDAW <medina.arnau@alumnes.ilerna.com>
2024-04-09 20:22:49 +0200

    Update

--- productos.js
+++ productos.js
@@ -1,19 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const filterButtons = document.querySelectorAll(".filter-button");
+    const filterCheckboxes = document.querySelectorAll(".filter-checkbox");
     const galleryImages = document.querySelectorAll(".gallery img");
 
-    filterButtons.forEach(button => {
-        button.addEventListener("click", function() {
-            const filterValue = this.getAttribute("data-filter");
+    filterCheckboxes.forEach(checkbox => {
+        checkbox.addEventListener("change", function() {
+            const checkedFilters = Array.from(filterCheckboxes)
+                .filter(checkbox => checkbox.checked)
+                .map(checkbox => checkbox.getAttribute("data-filter"));
 
-            // Remueve la clase 'active' de todos los botones
-            filterButtons.forEach(btn => btn.classList.remove("active"));
-            // Agrega la clase 'active' al botón clickeado
-            this.classList.add("active");
-
-            // Muestra u oculta las imágenes basadas en el filtro
             galleryImages.forEach(image => {
-                if (filterValue === "all" || image.classList.contains(filterValue)) {
+                const imageClasses = Array.from(image.classList);
+                if (checkedFilters.includes("all") || imageClasses.some(filter => checkedFilters.includes(filter))) {
                     image.style.display = "block";
                 } else {
                     image.style.display = "none";
